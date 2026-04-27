7280037c4b33a684a75a7eb63aa4cff0738bd5e5
Zahra Afifah <111202315009@mhs.dinus.ac.id>
2026-04-27 19:20:18 +0700

    Tugas 4

--- database/migrations/2026_04_02_114300_create_jadwal_periksa_table.php
+++ database/migrations/2026_04_02_114300_create_jadwal_periksa_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('jadwal_periksa', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_dokter')->constrained('users')->cascadeOnDelete();
-            $table->enum('hari', ['Senin', 'Selasa', 'Rabu' , 'Kamis' , 'Jumat' , 'Sabtu' , 'Minggu']);
+            $table->enum('hari', ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']);
             $table->time('jam_mulai');
-            $table->time('jam selesai');
+            $table->time('jam_selesai');
             $table->timestamps();
         });
     }
